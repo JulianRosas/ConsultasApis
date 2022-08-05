f196ca7be013541cae427ae934359c284a228901
JulianRosas <julymaxrosas@gmail.com>
2022-08-05 15:35:28 -0300

    Terminado?

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
-"""" 
-36e4086e5553fcc23dc6f3b3dab91113
-"""
-import requests, json
 
-api_key = "36e4086e5553fcc23dc6f3b3dab91113"
-base_url="http://api.openweathermap.org/data/2.5/weather?"
-city_name = input("ingresa el nombre de la ciudad: ")
-complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+import tkinter as tk
+import requests
 
-response=requests.get(complete_url)
-x=response.json()
-temperature =x["main"]["temp"]
-presion=x["main"]["pressure"]
-humedad=x["main"]["humidity"]
 
-print(temperature - 273.15," grados celcius en la ciudad de " + city_name,",un presion de",presion," hPa y una humedad del",humedad,"%")
-          
+ 
 
-import webbrowser
+def getWeather(root):
+    city = textField.get()
+    api = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=36e4086e5553fcc23dc6f3b3dab91113"
+    
+    json_data = requests.get(api).json()
+    condition = json_data['weather'][0]['main']
+    temp = int(json_data['main']['temp'] - 273.15)
+    pressure = json_data['main']['pressure']
+    humidity = json_data['main']['humidity']
+    wind = json_data['wind']['speed']
+    
 
-f = open('helloworld.html','w')
+    final_info = condition + "\n" + str(temp) + "°C" 
+    final_data = "\n" + "Pressure: " + str(pressure) + "\n" +"Humidity: " + str(humidity) + "\n" +"Wind Speed: " + str(wind) 
+    label1.config(text = final_info)
+    label2.config(text = final_data)
 
-message = """<html>
-<head></head>
-<body><p>Hello World!</p></body>
-</html>"""
 
-f.write(message)
-f.close()
+root = tk.Tk()
+root.geometry("600x500")
+root.title("Clima app")
+f = ("poppins", 15, "bold")
+t = ("poppins", 35, "bold")
 
-webbrowser.open_new_tab('openweatherAPI.html')
+textField = tk.Entry(root, justify='center', width = 20, font = t)
+textField.pack(pady = 20)
+textField.focus()
+textField.bind('<Return>', getWeather)
+
+label1 = tk.Label(root, font=t)
+label1.pack()
+label2 = tk.Label(root, font=f)
+label2.pack()
+root.mainloop()
